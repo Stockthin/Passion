c9ea03400614f6083a87cdae2678955a3d4d4b18
Hai Duong <stockthin@gmail.com>
2016-10-03 23:29:16 +0700

    Revert "111"
    This reverts commit 6fb4e109d8eab3f7a80788ab5f29aa4c72fd3460.

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connect = mysql_connect("localhost", "root", "") or die ("Server not found!");
-mysql_select_db("webphim", $connect) or die("Database not found!");
-mysql_query("SET NAMES 'utf8'");
-?>
